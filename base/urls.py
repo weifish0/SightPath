675c3f6ec186b1ec422d461d62cf8a59f88aabff
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-06-08 00:43:39 +0800

    增加討論室建立、修改功能

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("profile", views.profile, name="profile"),
     path("room/<str:pk>", views.room, name="room"),
     path("create_room", views.create_room, name="create_room"),
+    path("update_room/<str:pk>", views.update_room, name="update_room"),
     path("", views.home, name="home")
     
 ]
