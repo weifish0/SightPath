275126b1d06426e61ba6bfcf49f693a7140cd7af
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-06-17 22:09:26 +0800

    討論室CRUD

--- base/views.py
+++ base/views.py
@@ -20,6 +20,7 @@ def room(request,pk):
 
 def create_room(request):
     form = RoomForm()
+    context = {"form": form}
     # 使用者送出表單
     if request.method == "POST":
         # 在資料庫中新增資料
@@ -27,14 +28,14 @@ def create_room(request):
         if form.is_valid():
             # 符合格式就保存到資料庫，並且回到主頁
             form.save()
-            return redirect("home")
-    context = {"form": form}
+            return redirect("home")   
     return render(request, "base/room_form.html", context)
 
 def update_room(request, pk):
     room = Room.objects.get(id=pk)
     # 讓討論室抓取該討論室上次在資料庫存的資料
     form = RoomForm(instance=room)
+    context = {"form": form}
     if request.method == "POST":
         # 更新資料庫的資料
         form = RoomForm(request.POST, instance=room)
@@ -42,5 +43,12 @@ def update_room(request, pk):
             # 符合格式就保存到資料庫，並且回到主頁
             form.save()
             return redirect("home")
-    context = {"form": form}
     return render(request, "base/room_form.html", context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {"obj": room}
+    if request.method == "POST":
+        room.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", context)
