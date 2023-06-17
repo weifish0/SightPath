87c6049400aabbe9c0f0b5401a59bf971877371e
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-06-17 22:09:26 +0800

    討論室CRUD

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("room/<str:pk>", views.room, name="room"),
     path("create_room", views.create_room, name="create_room"),
     path("update_room/<str:pk>", views.update_room, name="update_room"),
+    path("delete_room/<str:pk>", views.delete_room, name="delete_room"),
     path("", views.home, name="home")
     
 ]
