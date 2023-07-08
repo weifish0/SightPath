22d2903c9dbb2c63ffd031e13eaa75538d09a168
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-07-08 10:46:05 +0800

    feat: 登入登出帳號功能

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("login", views.login_page, name="login_page"),
+    path("logout", views.logout_user, name="logout_user"),
     path("profile", views.profile, name="profile"),
     path("room/<str:pk>", views.room, name="room"),
     path("create_room", views.create_room, name="create_room"),
