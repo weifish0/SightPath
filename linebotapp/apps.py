39d56ee0ce14ac3fbe8f88671619c411530ae043
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-07-11 13:29:15 +0800

    style: 更改linebotapp package的名字

--- linebotapp/apps.py
+++ linebotapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SightpathlinebotappConfig(AppConfig):
+class LinebotappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "sightPathLineBotApp"
+    name = "linebotapp"
