731fe72300fdddeda5c4d4e8b28a584798f84290
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-07-17 09:42:06 +0800

    feat: 個人檔案修改

--- base/forms.py
+++ base/forms.py
@@ -1,10 +1,17 @@
 from django.forms import ModelForm
 from .models import Room
-
+from django.contrib.auth.models import User
 
 # 繼承django form的格式，詳細用法參考以下網址
 # https://docs.djangoproject.com/en/4.2/topics/forms/
 class RoomForm(ModelForm):
     class Meta:
         model = Room
+        fields = "__all__"
+        exclude = ["host", "participants"]
+        
+    
+class UserForm(ModelForm):
+    class Meta:
+        model = User
         fields = "__all__"
