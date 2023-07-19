8fba947d0ba0e48a7b91afcc1daedfca5dc9d83a
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-07-19 23:39:55 +0800

    feat: api

--- sightpath/urls.py
+++ sightpath/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("base.urls"))
+    path("", include("base.urls")),
+    path("api/", include("base.api.urls")),
 ]
