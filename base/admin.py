dfffdcfed03e2e5071fbed32c9afab65576dea15
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-07-24 21:04:48 +0800

    feat: add tags_fixture_generate.py

--- base/admin.py
+++ base/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Room, Topic, Message, Competition, User
+from .models import Room, Topic, Message, Competition, User, CompetitionTag
 
-    
+
+# class CompetitionTagAdmin(admin.ModelAdmin):
+#     list_display = ("id","tag_name")
+
 admin.site.register(Room)
 admin.site.register(Topic)
 admin.site.register(Message)
 admin.site.register(Competition)
 admin.site.register(User)
+admin.site.register(CompetitionTag)
