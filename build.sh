0db195c244d56dd0aa668ebe27409b07e915d47a
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-08-03 16:00:07 +0800

    chore: load fixtures data

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
+
+python ./base/fixtures/competitions_fixture_generate.py
+python manage.py loaddata ./base/fixtures/tags_fixture.json
+python manage.py loaddata ./base/fixtures/competitions_fixture.json
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
