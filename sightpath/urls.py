b98a50edf61c3e39f3b6475fd63acfa4fca6380f
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-08-22 22:53:16 +0800

    feat: line bot messaging api  basic setting and testing

--- sightpath/urls.py
+++ sightpath/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("base.urls")),
+    path("linebotapp/", include("linebotapp.urls")),
     path("api/", include("base.api.urls")),
 ]
 
