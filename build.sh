8b561e678f481cf33397b4072983ff43fbff58bb
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-09-12 00:00:35 +0800

    fix: edit the process of build.sh, makemigrations first

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+python manage.py makemigrations
 python manage.py migrate
+
 # python ./base/fixtures/competitions_fixture_generate.py
 # python manage.py flush
 python manage.py loaddata ./base/fixtures/tags_fixture.json
