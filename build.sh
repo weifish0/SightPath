8d2a1ec6eb17de79ed4bca6284cc17a70381561b
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-09-12 19:14:37 +0800

    chore: make migrations first and load web-crawler data

--- build.sh
+++ build.sh
@@ -3,12 +3,11 @@
 set -o errexit
 
 pip install -r requirements.txt
-
+python manage.py makemigrations
 python manage.py migrate
 # python ./base/fixtures/competitions_fixture_generate.py
-# python manage.py flush
-# python manage.py loaddata ./base/fixtures/tags_fixture.json
-# python manage.py loaddata ./base/fixtures/competitions_fixture.json
+python manage.py loaddata ./base/fixtures/tags_fixture.json
+python manage.py loaddata ./base/fixtures/competitions_fixture.json
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
