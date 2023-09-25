f74060f0f1535a1aab45136aa9b23fc8a2f77243
heyaustin <40731210+heyaustin@users.noreply.github.com>
2023-09-25 16:26:51 +0800

    tinder randomly pick 10~30 elements

--- build.sh
+++ build.sh
@@ -5,9 +5,13 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-# python ./base/fixtures/competitions_fixture_generate.py
+
+#########
+# python ./base/fixtures/competitions_fixture_generator.py
 python manage.py loaddata ./base/fixtures/tags_fixture.json
 python manage.py loaddata ./base/fixtures/competitions_fixture.json
+#########
+
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
