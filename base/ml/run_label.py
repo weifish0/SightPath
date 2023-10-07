59c48f10f47738a7e8df06d0d21c53c46cb59508
heyaustin <40731210+heyaustin@users.noreply.github.com>
2023-10-08 00:56:35 +0800

    update run_label

--- base/ml/run_label.py
+++ base/ml/run_label.py
@@ -4,7 +4,9 @@ import tags
 from text2vec import SentenceModel
 import re
 from sklearn.metrics.pairwise import cosine_similarity
+import opencc
 
+converter = opencc.OpenCC('tw2sp.json')
 CLEANR = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
 
 def cleanhtml(raw_html: str):
@@ -13,7 +15,7 @@ def cleanhtml(raw_html: str):
 
 if __name__ == "__main__":
     # 支持多语言的句向量模型（CoSENT），多语言（包括中英文）语义匹配任务推荐，支持fine-tune继续训练
-    model = SentenceModel("shibing624/text2vec-base-multilingual")
+    model = SentenceModel("GanymedeNil/text2vec-base-chinese")
     #pool = model.start_multi_process_pool()
     comp_path = os.getcwd()+'\\base\\fixtures\\competitions_fixture.json'
     tag_emb = []
@@ -28,25 +30,36 @@ if __name__ == "__main__":
     f.close()    
 
     for tag in df:
-        emb1 = model.encode(tag["fields"]["tag_name"])
+        emb1 = model.encode(converter.
+                            convert(tag["fields"]["tag_name"]))
         tag_emb.append(emb1.reshape(1, -1))
 
     i=0
-    for d in data:
+    for d in range(len(data)):
+        score_v=[]
         i+=1
-        print("rounds "+str(i)+'\n')
+        print("rounds "+str(i))
 
-        html = cleanhtml(d["fields"]["guide_line_html"])
-        emb2 = model.encode(d["fields"]["name"]+' '+html).reshape(1, -1)
-        d["fields"]["tags"] = []
+        html = cleanhtml(data[d]["fields"]["guide_line_html"])
+        emb2 = model.encode(converter.
+                            convert(data[d]["fields"]["name"])).reshape(1, -1)
+        # +' '+html
+        data[d]["fields"]["tags"] = []
 
         for pk in range(len(tag_emb)):
-            score = cosine_similarity(tag_emb[pk], emb2)[0][0]
-            #print(score)
+            #if pk+1==11 and i==290:
+            score = cosine_similarity(emb2, tag_emb[pk])[0][0]
+            if score>0.4:
+                data[d]["fields"]["tags"].append(pk+1)
+            #score_v.append(score)
 
-            if score>0.8:
-                d["fields"]["tags"].append(pk+1)
-                with open(comp_path, "w", encoding="utf-8") as fp:
-                    json.dump(data, fp, indent=2, ensure_ascii=False)
+        """
+        vec = sorted(score_v)
+        data[d]["fields"]["tags"].append(score_v.index(vec[-1])+1)
+        data[d]["fields"]["tags"].append(score_v.index(vec[-2])+1)
+        data[d]["fields"]["tags"].append(score_v.index(vec[-3])+1)
+        """
 
 
+    with open(comp_path, "w", encoding="utf-8") as fp:
+        json.dump(data, fp, indent=2, ensure_ascii=False)
