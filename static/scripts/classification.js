3e7880e5fbdd7c0ba21b9647e75ce210fd4505db
heyaustin <40731210+heyaustin@users.noreply.github.com>
2023-10-12 11:23:36 +0800

    classification.js

--- static/scripts/classification.js
+++ static/scripts/classification.js
@@ -1,4 +1,41 @@
-const model = tf.sequential({
+function getData(string) {
+    const request = indexedDB.open("model_data");
+    return new Promise(function (resolve, reject) {
+        request.onupgradeneeded = function (e) {
+            db = e.target.result;
+            console.log('running onupgradeneeded');
+            const store_love = db.createObjectStore('love');
+            const store_nope = db.createObjectStore('nope');
+            const store_tmp = db.createObjectStore('tmp');
+        };
+        request.onsuccess = function (e) {
+            let db = e.target.result;
+            let transaction = db.transaction(string, "readwrite"); // (1)
+            let items = transaction.objectStore(string);
+
+            let records = items.getAll();
+            records.onsuccess = function () {
+                let love = records.result;
+                resolve(love)
+            };
+            db.close();
+        }
+    });
+}
+
+function train() {
+    getData("love").then(function(result){
+        console.log("love:");
+        console.log(result);
+    });
+    getData("nope").then(function(result){
+        console.log("nope:");
+        console.log(result);
+    });
+}
+
+
+/* const model = tf.sequential({
     layers: [tf.layers.dense({ units: 1, inputShape: [10] })]
 });
 model.compile({ optimizer: 'sgd', loss: 'meanSquaredError' });
@@ -9,7 +46,7 @@ for (let i = 1; i < 5; i++) {
         epochs: 3
     });
     console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
-}
+} */
 
 
 /* const model = tf.sequential(
