e9149ae83d731966819bc320aa9f0c9b66915a36
heyaustin <40731210+heyaustin@users.noreply.github.com>
2023-10-12 14:27:24 +0800

    tinder.js score

--- static/scripts/classification.js
+++ static/scripts/classification.js
@@ -1,4 +1,4 @@
-function getData(string) {
+function getData(string, id = "no") {
     const request = indexedDB.open("model_data");
     return new Promise(function (resolve, reject) {
         request.onupgradeneeded = function (e) {
@@ -13,7 +13,10 @@ function getData(string) {
             let transaction = db.transaction(string, "readwrite"); // (1)
             let items = transaction.objectStore(string);
 
-            let records = items.getAll();
+            let records;
+            if (id == "no") records = items.getAll();
+            else records = items.get(id);
+
             records.onsuccess = function () {
                 let love = records.result;
                 resolve(love)
@@ -23,39 +26,66 @@ function getData(string) {
     });
 }
 
-function train() {
-    getData("love").then(function(result){
-        console.log("love:");
-        console.log(result);
-    });
-    getData("nope").then(function(result){
-        console.log("nope:");
-        console.log(result);
-    });
+const epoch_num = 1;
+
+async function train() {
+    let love = await getData("love");
+    let nope = await getData("nope");
+    let tmp_emb;
+    let love_emb = [];
+    let nope_emb = [];
+
+    for (let i = 0; i < love.length; i++) {
+        tmp_emb = await getData("tmp", love[i]);
+        love_emb.push(JSON.parse(tmp_emb["emb"]))
+    }
+    for (let i = 0; i < nope.length; i++) {
+        tmp_emb = await getData("tmp", nope[i]);
+        nope_emb.push(JSON.parse(tmp_emb["emb"]))
+    }
+
+    let model;
+    try {
+        model = await tf.loadLayersModel('indexeddb://model');
+        model.compile({
+            optimizer: 'adam',
+            loss: 'binaryCrossentropy'
+        });
+    } catch (error) {
+        console.log("indexeddb model not loaded")
+
+        model = tf.sequential({
+            layers: [tf.layers.dense({
+                units: 1,
+                inputShape: [shape]
+            })]
+        });
+        model.compile({
+            optimizer: 'adam',
+            loss: 'binaryCrossentropy'
+        });
+    }
+
+
+    for (let i = 0; i < epoch_num; i++) {
+        const h = await model.fit(
+            tf.tensor(love_emb),
+            tf.fill([love_emb.length, 1], 1), {
+            batchSize: 4,
+            epochs: 3
+        });
+        console.log("love Loss after Epoch " + i + " : " + h.history.loss[0]);
+    }
+    for (let i = 0; i < epoch_num; i++) {
+        const h = await model.fit(
+            tf.tensor(nope_emb),
+            tf.fill([nope_emb.length, 1], -1), {
+            batchSize: 4,
+            epochs: 3
+        });
+        console.log("nope Loss after Epoch " + i + " : " + h.history.loss[0]);
+    }
+
+    const saveResults = await model.save('indexeddb://model');
 }
 
-
-/* const model = tf.sequential({
-    layers: [tf.layers.dense({ units: 1, inputShape: [10] })]
-});
-model.compile({ optimizer: 'sgd', loss: 'meanSquaredError' });
-
-for (let i = 1; i < 5; i++) {
-    const h = await model.fit(tf.ones([8, 10]), tf.ones([8, 1]), {
-        batchSize: 4,
-        epochs: 3
-    });
-    console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
-} */
-
-
-/* const model = tf.sequential(
-    {layers: [tf.layers.dense({units: 1, inputShape: [3]})]});
-console.log('Prediction from original model:');
-model.predict(tf.ones([1, 3])).print();
-
-const saveResults = await model.save('indexeddb://my-model-1');
-
-const loadedModel = await tf.loadLayersModel('indexeddb://my-model-1');
-console.log('Prediction from loaded model:');
-loadedModel.predict(tf.ones([1, 3])).print(); */
