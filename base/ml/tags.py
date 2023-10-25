3b9ddfab792dd4763d7c554e37c32e4ebd78b58b
heyaustin <40731210+heyaustin@users.noreply.github.com>
2023-10-25 23:40:07 +0800

    recommendation system

--- base/ml/tags.py
+++ base/ml/tags.py
@@ -1,14 +1,17 @@
 import requests
 import json
 import os
-from datetime import datetime, timezone
+# from datetime import datetime, timezone
+from tags_description import *
 
-tags = ["繪畫比賽","平面設計比賽","產品設計比賽","綜合設計",
-        "攝影比賽","影片比賽",
-        "文學獎","創意寫作",
-        "企劃競賽","創業競賽","程式競賽",
-        "音樂大賽","歌唱比賽","詞曲創作",
-        "運動","選秀"]
+# "繪畫比賽","平面設計比賽","產品設計比賽","綜合設計",
+#         "攝影比賽","影片比賽",
+#         "文學獎","創意寫作",
+#         "企劃競賽","創業競賽","程式競賽",
+#         "音樂大賽","歌唱比賽","詞曲創作",
+#         "運動","選秀"
+
+        
 
 def generate_tags():
     json_path = os.getcwd()+'\\base\\fixtures\\tags_fixture.json'
@@ -17,7 +20,8 @@ def generate_tags():
     for i in range(len(tags)):
         output_tags_fixture.append({"model": "base.competitiontag",
                                     "pk": i+1,
-                                    "fields": {"tag_name": tags[i]}})
+                                    "fields": {"tag_name": tags[i],
+                                               "description": descriptions[i]}})
                     
     
     with open(json_path, "w", encoding="utf-8") as fp:
--- base/models.py
+++ base/models.py
@@ -126,7 +126,8 @@ class Message(models.Model):
     
 class CompetitionTag(models.Model):
     tag_name = models.CharField(max_length=50)
-        
+    description = models.CharField(max_length=400)
+    
     def __str__(self):
         return f"{self.tag_name}"
     
