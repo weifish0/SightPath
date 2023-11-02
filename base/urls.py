9ab924627d2de8d2779f4736f9c50df82a819e41
Will Cheng <98398911+weifish0@users.noreply.github.com>
2023-11-02 20:11:51 +0800

    feat: add new function enabling superuser to pin a post

--- base/urls.py
+++ base/urls.py
@@ -11,8 +11,12 @@ urlpatterns = [
     path("profile/<str:pk>/", views.profile, name="profile"),
     path("room/<str:pk>/", views.room, name="room"),
     path("create_room/", views.create_room, name="create_room"),
+    
     path("update_room/<str:pk>/", views.update_room, name="update_room"),
     path("delete_room/<str:pk>/", views.delete_room, name="delete_room"),
+    path("pin_room/<str:pk>/", views.pin_room, name="pin_room"),
+    path("unpin_room/<str:pk>/", views.unpin_room, name="unpin_room"),
+    
     path("delete_message/<str:pk>/", views.delete_message, name="delete_message"),
     path("edit_profile/<str:pk>/", views.edit_profile, name="edit_profile"),
     path("chatroom_home/", views.chatroom_home, name="chatroom_home"),
