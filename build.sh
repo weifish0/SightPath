c9fd23e2d1ea00e5239298626efe0306408fc068
heyaustin <40731210+heyaustin@users.noreply.github.com>
2023-11-06 23:56:33 +0800

    update_database

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 #########
 # python ./base/fixtures/competitions_fixture_generator.py
-python manage.py loaddata ./base/fixtures/competition_tags_fixture.json
-python manage.py loaddata ./base/fixtures/competitions_fixture.json
-python manage.py loaddata ./base/fixtures/ourtag_fixture.json
+python3 manage.py loaddata ./base/fixtures/competition_tags_fixture.json
+python3 manage.py loaddata ./base/fixtures/competitions_fixture.json
+python3 manage.py loaddata ./base/fixtures/ourtag_fixture.json
 # python manage.py loaddata ./base/fixtures/activities_fixture.json
 #########
 
